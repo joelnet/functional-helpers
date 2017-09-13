44e69d52baacf064443f792a93d971d173ff2ffd
Joel Thoms <jthoms@gmail.com>
2017-09-13 02:31:18 -0700

    bug fix for promisifyAll (missing keys)

--- promisifyAll.js
+++ promisifyAll.js
@@ -4,8 +4,16 @@ const shouldPromisify = (obj, key) =>
     obj[key] instanceof Function
     && obj[key + 'Async'] === undefined
 
+const keys = obj => {
+    const value = []
+    for (key in obj) {
+        value.push(key)
+    }
+    return value
+}
+
 module.exports = obj =>
-    Object.keys(obj)
+    keys(obj)
         .reduce((acc, key) => {
             if (shouldPromisify(obj, key)) {
                 acc[key] = promisify(obj[key], obj)
