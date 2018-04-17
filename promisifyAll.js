51a33fe010d40463b69fb5c9e69e7ba3f27c8e53
Joel Thoms <jthoms@gmail.com>
2018-04-16 21:48:24 -0700

    minor change to promisifyAll

--- promisifyAll.js
+++ promisifyAll.js
@@ -3,16 +3,8 @@ const promisify = require('./promisify')
 const shouldPromisify = (obj, key) =>
     typeof obj[key] === 'function'
 
-const keys = obj => {
-    const value = []
-    for (key in obj) {
-        value.push(key)
-    }
-    return value
-}
-
 module.exports = obj =>
-    keys(obj)
+    Object.keys(obj)
         .reduce((acc, key) => {
             if (shouldPromisify(obj, key)) {
                 Object.assign(acc, { [key]: promisify(obj[key], obj) })
