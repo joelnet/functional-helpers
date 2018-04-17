63a62c7d2e947a0a702b1b233371860fb98d15dd
Joel Thoms <jthoms@gmail.com>
2018-04-17 01:09:56 -0700

    100% code coverage

--- __tests__/promisify.tests.js
+++ __tests__/promisify.tests.js
@@ -2,21 +2,36 @@ const fs = require('fs')
 const promisify = require('../promisify')
 
 describe('promisify', () => {
-  test('with no arguments resolves', done => {
+  test('with no arguments resolves', () => {
     expect.assertions(1)
     const expected = {}
     const promise = promisify(callback => callback(null, expected))
 
     return promise()
       .then(data => expect(data).toBe(expected))
-      .then(done)
   })
 
-  test.only('calls callback with data', done => {
+  test('resolves', () => {
+    expect.assertions(1)
+    const promise = promisify((x, y, callback) => callback(null, x + y))
+    
+    return promise(2, 3)
+      .then(data => expect(data).toBe(5))
+  })
+
+  test('rejects', () => {
+    expect.assertions(1)
+    const promise = promisify((x, y, callback) => callback(x + y))
+    
+    return promise(2, 3)
+      .catch(err => expect(err).toBe(5))
+  })
+
+  test('calls callback with data', done => {
     expect.assertions(2)
     const promise = promisify((x, y, callback) => callback(null, x + y))
     
-    promise(2, 3, (err, data) => {
+    return promise(2, 3, (err, data) => {
       expect(err).toBe(null)
       expect(data).toBe(5)
       done()
@@ -37,7 +52,7 @@ describe('promisify', () => {
   test('works with fs', done => {
     expect.assertions(1)
     const readFile = promisify(fs.readFile)
-    const args = [process.cwd() + '/promisifyAll.js', 'utf8']
+    const args = [process.cwd() + '/LICENSE', 'utf8']
 
     fs.readFile(...args, (err, data) => {
       readFile(...args)
